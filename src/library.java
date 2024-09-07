c15b71873400980f533a1930bb100d1206911c31
Rewansh57 <rewanshchoudhary57@gmail.com>
2024-09-07 19:33:48 +0530

    just added change and remove functionality

--- src/library.java
+++ src/library.java
@@ -12,32 +12,21 @@ class enter_book{
 
 
 public void enter_details(){
-
     System.out.println("enter the bookid");
     book_Id=scn.nextInt();
 
-
-
     System.out.println("Enter the name of the author ");
     book_author=scn.next();
-
-
 
     System.out.println("enter the name of the book");
     book_name=scn.next();
 
 
 }}
-
 class head  {
     Scanner scn=new Scanner(System.in);
     enter_book ent=new enter_book();
-
-
-
     Map<Integer,String[]> books=new HashMap<>();
-
-
 
     public void add_book(){
         books.put(101, new String[]{"Author A", "Book A"});
@@ -45,6 +34,8 @@ class head  {
         books.put(103, new String[]{"Author C", "Book C"});
         books.put(104, new String[]{"Author D", "Book D"});
         books.put(105, new String[]{"Author E", "Book E"});
+        System.out.println("Enter the number of books to be entered");
+
 
         int repeat=scn.nextInt();
         for (int i=0;i<repeat;i++){
@@ -67,17 +58,89 @@ class head  {
     public void remove_book(){
         System.out.println("Enter the bookid to remove the book");
         int rnum=scn.nextInt();
-        for(Map.Entry<Integer,String[]> entry: )
+        if (books.containsKey(rnum)){
+                books.remove(rnum);
+
+            }
+            else {
+                System.out.println("The number you enter has no matching bookid");}
+            System.out.println("After the action taken by the user the reminibg collection:");
+            display_book(books);
+    }
+    public void change_entries(){
+        System.out.println("1. Changing the book Id");
+        System.out.println("2. Changing the book author");
+        System.out.println("3. Changing the bookname");
+        System.out.println("Enter choice that you want to make ");
+        int enter=scn.nextInt();
+
+        switch(enter){
+            case 1:
+                System.out.println("Enter the bookid you want to change");
+                int oldkey=scn.nextInt();
+
+                if (books.containsKey(oldkey)==true){
+                    System.out.println("Enter the bookid you want to add");
+                    int newkey=scn.nextInt();
+                    String [] values=books.get(oldkey);
+                    books.remove(oldkey);
+                    books.put(newkey,values);
+                    System.out.println("Your request is implemented");
+                    display_book(books);
+                }
+                else{
+                    System.out.println("The Id you entered is not present ");
+                }
+                break;
+            case 2 :
+                System.out.println("Enter the bookid of the book whose author's name you want to change");
+                int index=scn.nextInt();
+                if(books.containsKey(index)==true){
+                    String [] values=books.get(index);
+                    String old_value=values[0];
+                    System.out.println("The Author's name that you want");
+                    String new_value=scn.nextLine();
+                    String [] new_values={new_value,values[1]};
+                    books.put(index,new_values);
+                    System.out.println("Your request is implemented");
+                    display_book(books);
+                }
+                else{
+                    System.out.println("The bookid you gave does not exist");
+
+                }
+                break;
+            case 3:
+                System.out.println("Enter the bookId to change the name ");
+                int oldindex=scn.nextInt();
+                if(books.containsKey(oldindex)==true){
+                    String [] values=books.get(oldindex);
+                    System.out.println("Enter the new bookname");
+                    String name=scn.nextLine();
+
+                    String [] new_values={values[0],name};
+                    books.put(oldindex,new_values);
+                    System.out.println("The changes have been implemneted ");
+                    display_book(books);
+                }
+        }
+
+
+
+
+
 
     }
 }
-public class Main {
+public class library {
 
     public static void main(String[] args) {
         enter_book entb=new enter_book();
         head hd=new head();
         hd.add_book();
         hd.display_book(hd.books);
+        hd.remove_book();
+
 
 
 

da1b96d6a1bc028bfd42883b70e59a41043f77e3
Rewansh57 <rewanshchoudhary57@gmail.com>
2024-09-08 22:36:46 +0530

    This is a snap after implemneting sorting by book id

--- src/library.java
+++ src/library.java
@@ -1,7 +1,5 @@
 
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 class enter_book{
 
@@ -28,7 +26,7 @@ class head  {
     enter_book ent=new enter_book();
     Map<Integer,String[]> books=new HashMap<>();
 
-    public void add_book(){
+    public  void  add_book(){
         books.put(101, new String[]{"Author A", "Book A"});
         books.put(102, new String[]{"Author B", "Book B"});
         books.put(103, new String[]{"Author C", "Book C"});
@@ -71,6 +69,7 @@ class head  {
         System.out.println("1. Changing the book Id");
         System.out.println("2. Changing the book author");
         System.out.println("3. Changing the bookname");
+        System.out.println("4.Sorting the books in the order of book ID");
         System.out.println("Enter choice that you want to make ");
         int enter=scn.nextInt();
 
@@ -123,6 +122,222 @@ class head  {
                     System.out.println("The changes have been implemneted ");
                     display_book(books);
                 }
+                else{
+                    System.out.println("The bookid you gave does not exist");
+                }
+                break;
+            case 4:
+                sort_byID();
+
+        }
+
+    }
+    public void sort_byID(){
+        int [] values= new int [books.size()];
+        int i=0;
+        Map<Integer,String[]> sorted_books=new LinkedHashMap<>();
+
+        for(Map.Entry<Integer,String[]>entry: books.entrySet()){
+            values[i]=entry.getKey();
+            i++;
+        }
+        sorting(values);
+        for (int l =0;l<values.length;l++){
+            sorted_books.put(values[l],books.get(values[l]));
+
+        }
+        System.out.println("Here is you sorted books:------------------- "+"\n");
+        display_book(sorted_books);
+    }
+
+    public void sorting( int array[]){
+        int mainlen=array.length;
+        int [] leftside=new int[mainlen/2];
+        int [] rightside =new int[mainlen-leftside.length];
+        if (array.length<2){
+            return;
+        }
+        for (int i=0;i<mainlen;i++){
+            if (i<leftside.length){
+                leftside[i]=array[i];
+            }
+            else{
+                rightside[i- leftside.length]=array[i];
+
+            }
+        }
+        sorting(leftside);
+        sorting(rightside);
+        merging(array,leftside,rightside);
+    }
+    public void merging(int [] array,int[] leftarray,int[] rightarray){
+
+        int i=0,j=0,k=0;
+        while(i< leftarray.length&&j<rightarray.length){
+            if (leftarray[i]<=rightarray[j]){
+                array[k]=leftarray[i];
+                i++;
+            }
+            else{
+                array[k]=rightarray[j];
+                j++;
+
+            }
+            k++;
+
+
+        }
+        while(i<leftarray.length){
+            array[k]=leftarray[i];
+            i++;
+            k++;
+        }
+        while(j< rightarray.length){
+            array[k]= rightarray[j];
+            j++;
+            k++;
+        }
+    }
+
+}
+class student extends head{
+    public void fetch_books_byid(){
+        System.out.println("enter your choice ");
+
+        System.out.println("1.Search book by bookID");
+        System.out.println("2.Search book by author name");
+        System.out.println("3.Search book by book name");
+
+        int choice =scn.nextInt();
+        switch(choice){
+            case 1 :
+                System.out.println("Enter the book id you want to fetch ");
+                int index=scn.nextInt();
+                if (books.containsKey(index)==true){
+                    books.get(index);
+
+                }
+                else {
+                    System.out.println("Invalid book id ");
+
+                }
+                break;
+            case 2 :
+                System.out.println("Enter the author name of the book you want to fetch ");
+                String author_name=scn.nextLine();
+                String [] values=new String[books.size()];
+                int keyindex = -1;
+                
+
+
+                for (Map.Entry<Integer,String[]>entry: books.entrySet()){
+                    values= entry.getValue();
+                        if (values[0].equals(author_name)){
+                            keyindex= entry.getKey();
+                            break;
+                        }
+                        else{
+                            continue;
+                        }
+                }
+                if (keyindex!=-1){
+                    System.out.println("We found the book reuqested and the details are"+ Arrays.toString(books.get(keyindex)));
+                }
+                else{
+                    System.out.println("No such book was found");
+                }
+                break;
+            case 3:
+                System.out.println("Enter the name of the book you want ");
+                String name=scn.nextLine();
+                int keyindex2=-1;
+                String [] values2=new String[books.size()];
+                for (Map.Entry<Integer,String[]>entry:books.entrySet()){
+                    values2= entry.getValue();
+                    if(values2[1].equals(name)){
+                        keyindex2=entry.getKey();
+                        break;
+
+                    }
+                    else{
+                        continue;
+                    }
+
+                }
+                if (keyindex2!=-1){
+                    System.out.println("The books you searched fopr is :"+ Arrays.toString(books.get(keyindex2)));
+
+                }
+                else{
+                    System.out.println("The bookbname you entered does not exist");
+
+                }
+
+              }
+
+
+
+
+
+    }
+    public void issue_book(){
+        System.out.println("Enter the no of books to be issued");
+        int n=scn.nextInt();
+        HashMap<Integer,String[]> collection=new HashMap<>();
+        for (int i=0;i<n;i++){System.out.println("Enter the book id you want to fetch ");
+            int index=scn.nextInt();
+
+
+            if (books.containsKey(index)==true){
+                String [] values=books.get(index);
+                books.get(index);
+                books.remove(index);
+                collection.put(index,values);}
+            else{
+                System.out.println("The book id "+i+" you mentioned is invalid");
+            }
+
+
+        }
+        System.out.println("The remaining books");
+
+        display_book(books);
+        System.out.println(" The selected books");
+
+        display_book(collection);
+    }}
+
+
+public class library {
+
+    public static void main(String[] args) {
+        enter_book entb=new enter_book();
+        head hd=new head();
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter your choice For ex 1 2 3 4.......... ");
+        System.out.println("1.Head");
+        System.out.println("2. Student");
+        System.out.println("3. Employee");
+        int choice=scn.nextInt();
+        switch(choice){
+            case 1:
+                hd.add_book();
+                hd.display_book(hd.books);
+                System.out.println("Enter you choice");
+                System.out.println("1. To remove books");
+                System.out.println("2.To make changes to the collection ");
+                int choiceint=scn.nextInt();
+                if (choiceint==1){
+                    hd.remove_book();
+
+                }
+                else{
+                    hd.change_entries();
+                }
+                break;
+                //case 2
+
+
         }
 
 
@@ -130,16 +345,6 @@ class head  {
 
 
 
-    }
-}
-public class library {
-
-    public static void main(String[] args) {
-        enter_book entb=new enter_book();
-        head hd=new head();
-        hd.add_book();
-        hd.display_book(hd.books);
-        hd.remove_book();
 
 
 
